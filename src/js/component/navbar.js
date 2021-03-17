5d057df91c686862583a6cf5a85dfa98781270c5
Gustavo Ramírez <rngustavo@gmail.com>
2021-03-17 02:43:53 +0000

    inicio

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import logo from "../../img/Star_Wars_Logo.svg.png";
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">
+					<img className="logo" src={logo} />
+				</span>
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+
+			<div className="dropdown">
+				<button
+					className="btn btn-secondary dropdown-toggle"
+					type="button"
+					id="dropdownMenuButton2"
+					data-bs-toggle="dropdown"
+					aria-expanded="false">
+					Favorities
+				</button>
+				<ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+					<li>
+						<a className="dropdown-item active" href="#">
+							Action
+						</a>
+					</li>
+					<li>
+						<a className="dropdown-item" href="#">
+							Another action
+						</a>
+					</li>
+					<li>
+						<a className="dropdown-item" href="#">
+							Something else here
+						</a>
+					</li>
+				</ul>
 			</div>
 		</nav>
 	);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,64 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import rigoImage from "../../img/Star_Wars_Logo.svg.png";
 import "../../styles/home.scss";
 
 export const Home = () => (
-	<div className="text-center mt-5">
+	<div classNameName="text-center ">
 		<h1>Hello Rigo!</h1>
 		<p>
 			<img src={rigoImage} />
 		</p>
-		<a href="#" className="btn btn-success">
+
+		<a href="#" classNameName="btn btn-success">
 			If you see this green button, bootstrap is working
 		</a>
+		<div className="dropdown">
+			<button
+				className="btn btn-secondary dropdown-toggle"
+				type="button"
+				id="dropdownMenuButton"
+				data-toggle="dropdown"
+				aria-haspopup="true"
+				aria-expanded="false">
+				Dropdown button
+			</button>
+			<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+				<a className="dropdown-item" href="#">
+					Action
+				</a>
+				<a className="dropdown-item" href="#">
+					Another action
+				</a>
+				<a className="dropdown-item" href="#">
+					Something else here
+				</a>
+			</div>
+		</div>
+		{/* <div classNameName="btn-group">
+			<button
+				type="button"
+				classNameName="btn btn-danger dropdown-toggle"
+				data-bs-toggle="dropdown"
+				aria-expanded="false">
+				Action
+			</button>
+			<ul classNameName="dropdown-menu">
+				<li>
+					<a classNameName="dropdown-item" href="#">
+						Action
+					</a>
+				</li>
+				<li>
+					<a classNameName="dropdown-item" href="#">
+						Another action
+					</a>
+				</li>
+				<li>
+					<a classNameName="dropdown-item" href="#">
+						Something else here
+					</a>
+				</li>
+			</ul>
+		</div> */}
 	</div>
 );
